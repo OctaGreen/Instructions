13ab902909003d20b23894c98ca06e9567256dff
OctaGreen <anton_krotewich@mail.ru>
2017-07-27 14:13:28 +0300

    bootstrap rebuilt, database repaired

--- src/main/java/com/example/demo/model/Instruction.java
+++ src/main/java/com/example/demo/model/Instruction.java
@@ -3,15 +3,19 @@ package com.example.demo.model;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Instruction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
     @JsonView
     private String textinfo;
 
-    @Id
-    private Integer id;
     protected Instruction(){}
 
     public Instruction(String textinfo){
