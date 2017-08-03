f6fa6883b78155f08f7b4e461fa8787869405695
OctaGreen <anton_krotewich@mail.ru>
2017-08-03 16:39:58 +0300

    before creating heavy directives

--- src/main/webapp/WEB-INF/assets/app/app.js
+++ src/main/webapp/WEB-INF/assets/app/app.js
@@ -38,22 +38,68 @@ app.controller('AdminController',
     }
 );
 
+
+/*
+app.controller('CreateInstructionController', function($scope){
+    $scope.title = 'some title';
+    $scope.author = 'anton@itransition.com';
+    $scope.creationDate = '02.08.2017';
+    $scope.category = 'some category';
+    $scope.shortDescription = 'balblabla short description';
+    $scope.stepsList = [];
+
+
+    $scope.stepsButtons = [];
+     $scope.addStepBuilder = function($scope){
+     $scope.stepsButtons.push();
+     };
+    $scope.$watch('stepsList', function(model) {
+        $scope.instructionModelAsJson = angular.toJson(model, true);
+    }, true);
+});
+*/
+
 app.controller("StepController", function($scope) {
-    $scope.headline = "Example headline";
+    $scope.title = 'some title';
+    $scope.author = 'anton@itransition.com';
+    $scope.creationDate = '02.08.2017';
+    $scope.category = 'some category';
+    $scope.shortDescription = 'balblabla short description';
+    $scope.stepsList = [];
+
+    $scope.headline = "Example Headline";
+
     $scope.contentList = [];
     $scope.templates = [
         {type: "Text field", id: 1, message: "", position: null},
         {type: "Media field", id: 2, files: "", position: null}
     ];
-    $scope.contentList.push({type: "Text field", id: 3, message: "Example field. Replace it", position: 0});
 
+    $scope.contentList.push({type: "Text field", id: 3, message: "Example field. Replace it", position: 0});
+    $scope.stepsList.push({headline: $scope.headline, contentList: $scope.contentList});
+
+//Как указать на изменяемость переменной headline (выше)? Думаю, что $scope берет значения из конкретной узкой области видимости
+
+    $scope.addStepBuilder = function(){
+        $scope.stepsList.push({headline: $scope.headline, contentList: $scope.contentList});
+    };
 
     $scope.$watch('contentList', function(model) {
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
 
-
+    $scope.$watch('stepsList', function(model) {
+        $scope.instructionModelAsJson = angular.toJson(model, true);
+    }, true);
 });
+
+
+
+
+
+
+
+
 
 app.filter('markdown', function(){
     var converter = new Showdown.converter();
@@ -103,21 +149,23 @@ app.controller('uploadFileController', ['$scope', '$http', function($scope, $htt
         $http.post(url, data, config).success(function (response, status) {
             var restObj = JSON.parse(response);
             $scope.uploadResult=restObj.message;
-     /*       item.files.push(restObj.fileUrl);*/
             item.files = restObj.fileUrl;
         });
     };
 }]);
 
-app.controller('CreateInstructionController', function($scope){
-    $scope.title = 'some title';
-    $scope.author = 'anton@itransition.com';
-    $scope.creationDate = '02.08.2017';
-    $scope.category = 'some category';
-    $scope.shortDescription = 'balblabla short description';
-    $scope.stepsList = [];
-//stepsList = [{headline, content}, {headline, content}]
-    $scope.addStep = function($scope){
-        $scope.stepsList.push()
+app.directive('showStepbutton', [function(){
+    return{
+        restrict: 'AE',
+        replace: false,
+        template: '<div class="row"><div class="col-sm-12"><button class="btn btn-info" data-toggle="collapse" data-target="#showStepAction" ng-bind="headline" style="width: 100%"></button></div></div>'
+    }
+}]);
+
+app.directive('showStep', function(){
+    return{
+        restrict: 'AE',
+        replace: false
+
     }
 });
